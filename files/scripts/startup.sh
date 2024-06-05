63603affd365ceec927fa49eb0a3a76cb980bf2d
neomn110 <neomn110@gmail.com>
2024-06-05 13:04:06 +0330

    feat: update aliases

--- files/scripts/startup.sh
+++ files/scripts/startup.sh
@@ -3,4 +3,5 @@ eval $(ssh-agent)
 ssh-add /files/secrets/ssh_key
 export ANSIBLE_CONFIG=/ansible/ansible.cfg
 export $(grep -v '^#' /files/secrets/credentials.txt |xargs)
+cat /files/scripts/alias | tee > /root/.config/fish/config.fish
 fish
