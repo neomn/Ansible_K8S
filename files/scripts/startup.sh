93f5aa43d174e8f777df3e8eced7391f2461f246
neomn110 <neomn110@gmail.com>
2024-06-08 15:35:37 +0330

    fix: fix startup script issue with fish config directory

--- files/scripts/startup.sh
+++ files/scripts/startup.sh
@@ -3,5 +3,6 @@ eval $(ssh-agent)
 ssh-add /files/secrets/ssh_key
 export ANSIBLE_CONFIG=/ansible/ansible.cfg
 export $(grep -v '^#' /files/secrets/credentials.txt |xargs)
+fish -c "echo run a fish command to create fish config directory"
 cat /files/scripts/alias | tee > /root/.config/fish/config.fish
 fish
